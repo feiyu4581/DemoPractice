cd114cb1f54fe8c541578c04accabc3693d7bed8
zhengxiang <feiyu4581@users.noreply.github.com>
2021-06-20 17:20:27 +0800

    fix(array): 修改数组实现的变量名

--- 算法/array.py
+++ 算法/array.py
@@ -1,66 +1,66 @@
 
 class Array(object):
-    '''
+    """
     数组使用，支持以下方法
     @find：查询
     @insert：插入
     @append：追加
     @remove：移除
     @pop：弹出
-    '''
-    MIN_CAPICITY = 20
+    """
+    MIN_CAPACITY = 20
 
-    def __init__(self, capicity=MIN_CAPICITY):
+    def __init__(self, capacity=MIN_CAPACITY):
         self.length = 0
-        self.size = max(capicity, self.MIN_CAPICITY)
+        self.size = max(capacity, self.MIN_CAPACITY)
         self.vector = [None] * self.size
 
     def find(self, target):
-        for index in range(0, self.length):
-            if self.vector[index] == target:
-                return index
+        for i in range(0, self.length):
+            if self.vector[i] == target:
+                return i
 
         return -1
 
-    def check_capicity(self):
+    def check_capacity(self):
         # 当空间不足的时候，创建一个更大的空间，将过去的数据复制过去
         if self.length == self.size:
             self.size = self.size * 2 + 1
             old_vector = self.vector
             self.vector = [None] * self.size
-            for index in range(0, self.length):
-                self.vector[index] = old_vector[index]
+            for i in range(0, self.length):
+                self.vector[i] = old_vector[i]
 
             del old_vector
 
     def append(self, val):
-        self.check_capicity()
+        self.check_capacity()
         self.vector[self.length] = val
         self.length += 1
         return self.length - 1
  
-    def insert(self, index, val):
-        if index >= self.length or index < 0:
+    def insert(self, position, val):
+        if position >= self.length or position < 0:
             raise KeyError('Unbound Error')
 
-        self.check_capicity()
-        for index in range(self.length, index, -1):
-            self.vector[index] = self.vector[index - 1]
+        self.check_capacity()
+        for i in range(self.length, position, -1):
+            self.vector[i] = self.vector[i - 1]
 
-        self.vector[index] = val
+        self.vector[position] = val
         self.length += 1
-        return index
+        return position
 
     def remove(self, val):
-        index = self.find(val)
-        if index < 0:
+        position = self.find(val)
+        if position < 0:
             return -1
 
-        for index in range(index, self.length - 1):
-            self.vector[index] = self.vector[index] + 1
+        for i in range(position, self.length - 1):
+            self.vector[i] = self.vector[i + 1]
 
         self.length -= 1
-        return index
+        return position
 
     def pop(self):
         if self.length == 0:
