57bfc9070211d27d1d22537380423bfef9f2a7a7
zhuzx <zhuzx@ersoft.cn>
2021-07-01 20:01:44 +0800

    fix(array): 添加基数排序

--- 算法/排序/__init__.py
+++ 算法/排序/__init__.py
@@ -6,15 +6,17 @@ import random
 # from merge_sort import sort
 # from quick_sort import sort, choose_k_nums
 # from bucket_sort import sort
-from counting_sort import sort
+# from counting_sort import sort
+from radix_sort import sort
 
 ok = True
 for i in range(100):
-    datas = [random.randint(1, 100) for _ in range(100)]
-    # print(f'Before: {datas}')
+    # datas = [random.randint(1, 100) for _ in range(100)]
+    datas = [''.join([str(random.randint(0, 9)) for _ in range(10)]) for j in range(100)]
+    print(f'Before: {datas}')
     if list(sorted(datas)) != sort(datas):
         ok = False
 
-    # print(f'After: {sort(datas)}')
+    print(f'After: {sort(datas)}')
 
 print(f'After = Before: {ok}')
--- 算法/排序/counting_sort.py
+++ 算法/排序/counting_sort.py
@@ -1,9 +1,9 @@
-def sort(items):
-    max_data = max(max(items) + 1, 10)
+def sort(items, key=lambda item: item):
+    max_data = max(max([key(item) for item in items]) + 1, 10)
 
     buckets = [[] for _ in range(max_data)]
     for item in items:
-        buckets[item].append(item)
+        buckets[key(item)].append(item)
 
     res = []
     for bucket in buckets:
