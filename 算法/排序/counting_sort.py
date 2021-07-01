c9aceddf7cd0fa5f76f0eca075a81c5a5e62d756
zhuzx <zhuzx@ersoft.cn>
2021-07-01 20:13:36 +0800

    fix(array): 添加使用位置定义方式的计数排序

--- 算法/排序/counting_sort.py
+++ 算法/排序/counting_sort.py
@@ -10,3 +10,21 @@ def sort(items, key=lambda item: item):
         res.extend(bucket)
 
     return res
+
+
+def sort_with_counting(items, key=lambda item: item):
+    max_data = max(max([key(item) for item in items]) + 1, 10)
+
+    buckets = [0 for _ in range(max_data)]
+    for item in items:
+        buckets[key(item)] += 1
+
+    for index in range(1, len(buckets)):
+        buckets[index] += buckets[index - 1]
+
+    res = [None for _ in range(len(items))]
+    for item in reversed(items):
+        buckets[key(item)] -= 1
+        res[buckets[key(item)]] = item
+
+    return res
